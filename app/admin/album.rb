631afafa0ea67952c2c370be2c91985b474fa375
nopunguyen <ngocphu.uit@gmail.com>
2017-08-01 22:31:18 +0700

    custom view

--- app/admin/album.rb
+++ app/admin/album.rb
@@ -4,6 +4,7 @@ ActiveAdmin.register Album do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 permit_params :title, :description, :status, :image, :top_flg, :width
+config.per_page = 10
 #
 # or
 #
@@ -12,6 +13,25 @@ permit_params :title, :description, :status, :image, :top_flg, :width
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  index pagination_total: false do
+    selectable_column
+    id_column
+    column :title
+    column :description
+    column :status
+    column "Image" do |album|
+      image_tag album.image, class: 'my_image_size'
+    end
+    column :top_flg
+    actions
+    # render "some_partial"
+  end
+
+  # /admin/albums/:id/images
+  member_action :images do
+    @images = resource.images
+    # This will render app/views/admin/albums/images.html.erb
+  end
 
 
 end
